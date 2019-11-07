611b894d73cbb1ffaef84847403d23bfeb6aab60
Taiye Salami <taiye41@gmail.com>
2019-11-07 22:14:22 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,3 +4,13 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [
+  [0,1,2]
+  [3,4,5]
+  [6,7,8]
+  [0,3,6]
+  [8,5,2]
+  [0,4,8]
+  [6,4,2]
+  [1,4,7]
+  ]
